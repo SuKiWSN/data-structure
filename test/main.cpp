797bce1b0ce93db5c68d912a75f2532c8768d17b
wanghanyu <27796554@qq.com>
2022-08-13 21:04:06 +0800

    123

--- test/main.cpp
+++ test/main.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 #define MaxSize 50
 
+//线性表结构体
 typedef struct {
     int data[MaxSize];
     int length;
 }SqlList;
 
+//线性表插入
 bool ListInsert(SqlList &L, int i, int e){
     if (i<1 || i>L.length+1){
         return false;
@@ -20,12 +22,20 @@ bool ListInsert(SqlList &L, int i, int e){
         L.data[j] = L.data[j-1];
     }
     L.data[i-1] = e;
-//    cout<<L.length<<endl;
     L.length++;
     return true;
 }
 
-bool ListDelete(SqlList &L, int i, int e){
+//线性表删除
+bool ListDelete(SqlList &L, int i, int &e){
+    if (i<1 || i>L.length){
+        return false;
+    }
+    e = L.data[i-1];
+    for (int j=i;j<L.length;j++){
+        L.data[j-1] = L.data[j];
+    }
+    L.length--;
     return true;
 }
 
@@ -33,11 +43,16 @@ int main(){
     SqlList L;
     L.length = 0;
     int i = 1;
-    int e = 3;
-    ListInsert(L, i, e);
-    ListInsert(L, i+1, e+1);
-    cout<<L.length<<endl<<L.data[0]<<endl<<L.data[1]<<endl;
+    int e[5] = {1, 2, 3, 4, 5};
+    int x;
+    for (;i<5;i++){
+        ListInsert(L, i, e[i-1]);
+    }
     
+    cout<<L.length<<endl<<L.data[0]<<endl<<L.data[1]<<endl;
+    ListDelete(L, 2, x);
+    cout<<endl;
+    cout<<L.length<<endl<<L.data[1]<<endl;
     
     return 0;
 }
