dcbb3bcc204af9b7c64896023d6e6bfb3bec5009
wanghanyu <27796554@qq.com>
2022-08-13 22:09:15 +0800

    125

--- test/main.cpp
+++ test/main.cpp
@@ -1,62 +1,11 @@
 #include <iostream>
-using namespace std;
 #include <stdio.h>
+using namespace std;
 
-#define MaxSize 50
+#include "lineform.h" //线性表
 
-//线性表结构体
-typedef struct {
-    int data[MaxSize];
-    int length;
-}SqlList;
-
-//线性表插入
-bool ListInsert(SqlList &L, int i, int e){
-    if (i<1 || i>L.length+1){
-        return false;
-    }
-    if (L.length>=MaxSize){
-        return false;
-    }
-    for (int j=L.length;j>=i;j--){
-        L.data[j] = L.data[j-1];
-    }
-    L.data[i-1] = e;
-    L.length++;
-    return true;
-}
-
-//线性表删除
-bool ListDelete(SqlList &L, int i, int &e){
-    if (i<1 || i>L.length){
-        return false;
-    }
-    e = L.data[i-1];
-    for (int j=i;j<L.length;j++){
-        L.data[j-1] = L.data[j];
-    }
-    L.length--;
-    return true;
-}
-int LocateElem(SqlList L, int e){
-    int i;
-    for (i=0;i<L.length;i++){
-        if (L.data[i] == e){
-            return i+1;
-        }
-    }
-    return 0;  //没找到，返回0
-}
-
-void printList(SqlList L){
-    for (int j=0;j<L.length;j++){
-        cout<<L.data[j]<<' ';
-    }
-    cout<<endl;
-}
-
-int main(){
-    SqlList L;
+void LineFormTest(){
+    LineForm L;
     L.length = 0;
     int i = 1;
     int e[5] = {1, 2, 3, 4, 5};
@@ -68,6 +17,14 @@ int main(){
     ListDelete(L, 2, x);
     printList(L);
     cout<<LocateElem(L, 5)<<endl;
+}
+
+
+int main(){
+/*
+ 测试
+ */
+    LineFormTest();
     
     return 0;
 }
