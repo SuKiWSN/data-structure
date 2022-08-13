a9dd020e6571113dba4b5fe3b33343624551611b
wanghanyu <27796554@qq.com>
2022-08-13 20:42:42 +0800

    second commit

---
+++ test/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+
+#define MaxSize 50
+
+typedef struct {
+    int data[MaxSize];
+    int length;
+}SqlList;
+
+bool ListInsert(SqlList &L, int i, int e){
+    if (i<1 || i>L.length+1){
+        return false;
+    }
+    if (L.length>=MaxSize){
+        return false;
+    }
+    for (int j=L.length;j>=i;j--){
+        L.data[j] = L.data[j-1];
+    }
+    L.data[i-1] = e;
+//    cout<<L.length<<endl;
+    L.length++;
+    return true;
+}
+
+int main(){
+    SqlList L;
+    L.length = 0;
+    int i = 1;
+    int e = 3;
+    ListInsert(L, i, e);
+    ListInsert(L, i+1, e+1);
+    cout<<L.length<<endl<<L.data[0]<<endl<<L.data[1]<<endl;
+    
+    
+    return 0;
+}
